7809501d2176080364c2fdad9b6874d02b01785b
Bernardo Bonifácio <xbernardofb@outlook.com>
2023-05-24 15:27:18 -0300

    Adicionado conteúdos nos atributos alt e alterado o estilo dos links na section projetos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BiografiaComponent } from './biografia/biografia.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { ExperienciasComponent } from './experiencias/experiencias.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CertificadosComponent } from './certificados/certificados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'biografia', title: 'Biografia', component: BiografiaComponent },
+  { path: 'projetos', title: 'Projetos', component: ProjetosComponent },
+  { path: 'experiencias', title: 'Experiências', component: ExperienciasComponent },
+  { path: 'skills', title: 'Skills', component: SkillsComponent },
+  { path: 'certificados', title: 'Certificados', component: CertificadosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
